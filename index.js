acafa5d0fe01cd04ce7d8e12245bc403f5d7f6af
bill-hennessey <bhennessey1020@gmail.com>
2021-08-25 20:15:38 -0600

    more JS code. Completed questions with branching, teamArray compiled

--- index.js
+++ index.js
@@ -1,31 +1,135 @@
 const inquirer = require("inquirer");
-const jest = require("jest");
+// const jest = require("jest");
 const Employee = require("./employee");
 const Manager = require("./manager");
 const Engineer = require("./engineer");
 const Intern = require("./intern");
-const team = [];
+const teamArray = [];
+const idArray = [];
 
-// Inquirer questions array looks like this below. Might need separate arrays for each job profile. In that case start with a soimple one question promt that leads to an if statement. Fire off another function with prompt questions based on the asnwer.
-const questions = [
+// questions for all employees
+const mQuestions = [
   {
     type: "input",
-    name: "firstName",
+    name: "mgrName",
+    message: "Please enter your first name",
+  },
+  {
+    type: "input",
+    name: "mgrEmail",
+    message: "Please enter your email",
+  },
+  {
+    type: "input",
+    name: "mgrID",
+    message: "Please enter your employee id number",
+  },
+  {
+    type: "input",
+    name: "mgrOfficeNum",
+    message: "Please enter your office number",
+  },
+];
+
+const empQuestions = [
+  {
+    type: "list",
+    name: "role",
+    message: "Please select the employee job role",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+  {
+    type: "input",
+    name: "empFirstName",
     message: "Please enter the employee first name",
   },
   {
     type: "input",
-    name: "email",
+    name: "empEmail",
     message: "Please enter the employee email",
   },
   {
     type: "input",
-    name: "github",
-    message: "Please enter the employee GitHub username",
+    name: "empID",
+    message: "Please enter the employee id number",
   },
   {
     type: "input",
-    name: "description",
-    message: "Please enter a description of your project",
+    name: "school",
+    message: "Please enter their school",
+    when: (input) => {
+      return input.role == "Intern";
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Please enter their GitHub username",
+    when: (input) => {
+      return input.role == "Engineer";
+    },
+  },
+  {
+    type: "confirm",
+    name: "moreEmployees",
+    message: "Would you like to enter more employees?",
   },
 ];
+
+const init = () => {
+  inquirer.prompt(mQuestions).then((data) => {
+    console.log(data);
+
+    const mgr = new Manager(
+      data.mgrName,
+      data.mgrID,
+      data.mgrEmail,
+      data.mgrOfficeNum
+    );
+    teamArray.push(mgr);
+    console.log(mgr);
+    console.log(teamArray);
+
+    memberObj(data);
+
+    return;
+  });
+};
+
+const memberObj = (data) => {
+  inquirer.prompt(empQuestions).then((data) => {
+    switch (data.role) {
+      case "Engineer":
+        const eng = new Engineer(
+          data.empFirstName,
+          data.empID,
+          data.empEmail,
+          data.github
+        );
+        teamArray.push(eng);
+        console.log(eng);
+        console.log(teamArray);
+        break;
+      case "Intern":
+        const int = new Intern(
+          data.empFirstName,
+          data.empID,
+          data.empEmail,
+          data.school
+        );
+        teamArray.push(int);
+        console.log(int);
+        console.log(teamArray);
+        break;
+    }
+    if (data.moreEmployees) {
+      memberObj(data);
+    }
+  });
+};
+
+init();
+
+// console.log(init);
+
+// console.log(data);
